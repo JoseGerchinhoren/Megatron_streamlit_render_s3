af53f95d322e8cb350a62b659380ca8bec81d11c
JoseGerchinhoren <josegerchinhoren97@gmail.com>
2023-12-09 15:19:23 -0300

    .

--- visualizaPedidosFundas.py
+++ visualizaPedidosFundas.py
@@ -5,22 +5,10 @@ import boto3
 import os
 import io
 import datetime
+from config import cargar_configuracion
 
-# # Cargar configuración desde el archivo config.json
-# with open("../config.json") as config_file:
-#     config = json.load(config_file)
-
-# # Desempaquetar las credenciales desde el archivo de configuración
-# aws_access_key = config["aws_access_key"]
-# aws_secret_key = config["aws_secret_key"]
-# region_name = config["region_name"]
-# bucket_name = config["bucket_name"]
-
-# Configura tus credenciales y la región de AWS desde variables de entorno
-aws_access_key = os.getenv('aws_access_key_id')
-aws_secret_key = os.getenv('aws_secret_access_key')
-region_name = os.getenv('aws_region')
-bucket_name = 'megatron-accesorios'
+# Obtener credenciales
+aws_access_key, aws_secret_key, region_name, bucket_name = cargar_configuracion()
 
 # Conecta a S3
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=region_name)
